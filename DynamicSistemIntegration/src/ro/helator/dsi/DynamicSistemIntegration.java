0462c024c11e8dca2e7c625cc1e9e7efbc81f8d1
Bogdan <heim.bogdan@hotmail.com>
2016-06-20 18:38:16 +0300

    Now able to import wsdl and extract xsd from it if declared locally.

--- DynamicSistemIntegration/src/ro/helator/dsi/DynamicSistemIntegration.java
+++ DynamicSistemIntegration/src/ro/helator/dsi/DynamicSistemIntegration.java
@@ -1,14 +1,14 @@
 package ro.helator.dsi;
 
 import java.io.File;
-
 import ro.helator.dsi.webservice.WebServiceExtractor;
 
 public class DynamicSistemIntegration {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		File wsdl = WebServiceExtractor
-				.getWSDLFromURL("http://www.webservicex.net/geoipservice.asmx", "geoipservice");
+				.getWSDLFromURL("http://www.webservicex.net/geoipservice.asmx", null);
 		
+		WebServiceExtractor.extractXSDfromWSDL(wsdl);
 	}
 }
